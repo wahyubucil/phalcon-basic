8757878c9a4036d1c26c5a798dfb84e7ce7d7a5a
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-05-31 16:48:03 +0800

    Create dependency injection

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 
 use Phalcon\Loader;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Mvc\Application;
 
 // Define some absolute path constants to aid in locating resources
 define('BASE_PATH', dirname(__DIR__));
@@ -16,4 +20,36 @@ $loader->registerDirs(
 );
 
 $loader->register();
+
+// Create a DI
+$di = new FactoryDefault();
+
+// Setup the view component
+$di->set(
+    'view',
+    function() {
+        $view = new View();
+        $view->setViewsDir(APP_PATH . '/views');
+        return $view;
+    }
+);
+
+$di->set(
+    'url',
+    function() {
+        $url = new UrlProvider();
+        $url->setBaseUri('/');
+        return $url;
+    }
+);
+
+$application = new Application($di);
+try {
+    // Handle the request
+    $response = $application->handle();
+    
+    $response->send();
+} catch (\Exception $e) {
+    echo 'Exception: ', $e->getMessage();
+}
 ?>
